7110bbc074e29b86b9290cbdd3f02e433daf437d
Diego Estrada <diego.estrada1705@gmail.com>
2026-06-10 16:03:08 -0700

    hero section.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import hero from './assets/image/IMG-20260311-WA00411781055892.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <div class="logo">
             D'MERAKI
         </div>
@@ -16,6 +16,32 @@ function App() {
             <a href="">Contact</a>
         </nav>
       </header>
+      <main>
+        <section class="hero">
+          <div class="hero-content">
+            <div class="hero-text">
+              <span class="subtitle">BIENVENIDO A LA TRANQUILIDAD</span>
+              <h1>
+                Artesanos del <br></br>
+                <em>Bienestar y Alma</em>
+              </h1>
+              <p>
+              En D'MERAKI, fusionamos la ciencia del rejuvenecimiento
+              con el alma del cuidado artesanal. Un espacio diseñado
+              para restaurar tu equilibrio y realzar tu belleza natural.
+              </p>
+              <div class="buttons">
+                <a href="" class="btn btn-primary">Explorar Servicios</a>
+                <a href="" class="btn btn-outline">Contáctanos</a>
+              </div>
+            </div>
+            <div class="hero-image">
+              <img src={hero} alt="Spa Wellness"/>
+            </div>
+          </div>
+        </section>
+      </main>
+      
     </div>
   );
 }
